ba01f9fe17d6d293aff1642516d3896fd05bc8a0
Ant-Tonmay <tonmaysardar500@gmail.com>
2024-11-06 00:54:15 +0530

    Added Subject Route

--- src/main/java/live/thelocalhost/preplet_backend_v2/controller/SubjectManager.java
+++ src/main/java/live/thelocalhost/preplet_backend_v2/controller/SubjectManager.java
@@ -1,14 +1,12 @@
 package live.thelocalhost.preplet_backend_v2.controller;
 
 import live.thelocalhost.preplet_backend_v2.dto.SubjectDto;
+import live.thelocalhost.preplet_backend_v2.mapper.SubjectMapper;
 import live.thelocalhost.preplet_backend_v2.service.SubjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/subject")
@@ -20,5 +18,10 @@ public class SubjectManager {
     public ResponseEntity<SubjectDto> create(@RequestBody SubjectDto subjectDto){
         SubjectDto subjectDto1 = subjectService.create(subjectDto);
         return new ResponseEntity<>(subjectDto1, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/by-id")
+    public ResponseEntity<SubjectDto> getBySubjectId(@RequestParam Long id){
+        return new ResponseEntity<>(SubjectMapper.toDto(subjectService.getSubjectById(id)),HttpStatus.OK);
     }
 }
