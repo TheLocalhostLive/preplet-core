5401edd78585e807500dce029747645513fd9dde
Ant-Tonmay <tonmaysardar500@gmail.com>
2024-11-05 19:53:48 +0530

    Initial Commit

---
+++ src/main/java/live/thelocalhost/preplet_backend_v2/PrepletBackendV2Application.java
@@ -0,0 +1,13 @@
+package live.thelocalhost.preplet_backend_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrepletBackendV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrepletBackendV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/live/thelocalhost/preplet_backend_v2/PrepletBackendV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package live.thelocalhost.preplet_backend_v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrepletBackendV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
